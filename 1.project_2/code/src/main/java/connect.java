af13261434fe317d980cf75660175383977a0150
jessie <374649328@qq.com>
2021-11-20 19:44:32 +0800

    完善代码

---
+++ 1.project_2/code/src/main/java/connect.java
@@ -0,0 +1,38 @@
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+
+public class connect {
+    public static void main(String[] args) {
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "199915"));
+        Session session = driver.session();
+        String person_cypher = "CALL apoc.periodic.iterate('CALL apoc.load.csv(\\'./hw2/person.csv\\') yield map as row return row',\n" +
+                "                                'CREATE (person:PERSON) SET person = row', \n" +
+                "                                {batchSize:10000, iterateList:true, parallel:true})";
+        session.run(person_cypher);
+        String phone_cypher = "CALL apoc.periodic.iterate('CALL apoc.load.csv(\\'./hw2/phone2phone.csv\\') yield map as row return row',\n" +
+                "                                'CREATE (phone:PHONE) SET phone = row', \n" +
+                "                                {batchSize:10000, iterateList:true, parallel:true})";
+        session.run(phone_cypher);
+        String apply_cypher = "CALL apoc.periodic.iterate('CALL apoc.load.csv(\\'./hw2/apply_train.csv\\') yield map as row return row',\n" +
+                "                                'CREATE (apply:APPLY) SET apply = row', \n" +
+                "                                {batchSize:10000, iterateList:true, parallel:true})";
+        session.run(apply_cypher);
+        String blackinfo_cypher = "CALL apoc.periodic.iterate('CALL apoc.load.csv(\\'./hw2/phone.csv\\') yield map as row return row',\n" +
+        "                                'CREATE (blackinfo:BLACKINFO) SET blackinfo = row', \n" +
+        "                                {batchSize:10000, iterateList:true, parallel:true})";
+        session.run(blackinfo_cypher);
+
+        String relation_cypher = "MATCH(n:PERSON),(m:PHONE) where n.phone = m.from  merge(n)<-[r:RECEIVE]-(m) return r";
+        session.run(relation_cypher);
+        String relation_cypher2 = "MATCH(n:PERSON),(m:PHONE) where n.phone = m.to  merge(n)-[c:CALL]->(m) return c";
+        session.run(relation_cypher2);
+        String relation_cypher3 = "MATCH(n:PERSON),(m:APPLY) where n.id = m.applicant  merge(n)<-[c:PERSON_INFO]-(m) return c";
+        session.run(relation_cypher3);
+        String relation_cypher4 = "MATCH(n:PERSON),(m:BLACKINFO) where n.phone = m.number  merge(n)-[c:CHECK_BLACK]->(m) return c";
+        session.run(relation_cypher4);
+        session.close();
+        driver.close();
+    }
+}
--- 1.project_2/data_insert.sql
+++ 1.project_2/data_insert.sql
@@ -8,25 +8,26 @@
 
 -- #apoc语法，Connect工程文件也可使用, APOC语法的导入速度更快, 属性也不用单独配置
 -- #导入实体和属性
-    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./data/person.csv\') yield map as row return row',
+    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./hw2/person.csv\') yield map as row return row',
                                 'CREATE (person:PERSON) SET person = row', 
                                 {batchSize:10000, iterateList:true, parallel:true})
 
-    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./data/phone.csv\') yield map as row return row',
+    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./hw2/phone2phone.csv\') yield map as row return row',
                                 'CREATE (phone:PHONE) SET phone = row', 
                                 {batchSize:10000, iterateList:true, parallel:true})
 
-    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./data/apply_train.csv\') yield map as row return row',
+    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./hw2/apply_train.csv\') yield map as row return row',
                                 'CREATE (apply:APPLY) SET apply = row', 
                                 {batchSize:10000, iterateList:true, parallel:true})
 
-    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./data/phone2phone.csv\') yield map as row return row',
-                            'CREATE (phoneinfo:PHONEINFO) SET phoneinfo = row', 
+    CALL apoc.periodic.iterate('CALL apoc.load.csv(\'./hw2/phone.csv\') yield map as row return row',
+                            'CREATE (blackinfo:BLACKINFO) SET blackinfo = row', 
                             {batchSize:10000, iterateList:true, parallel:true})
 
 -- 导入关系
   MATCH(n:PERSON),(m:PHONE) where n.phone = m.from  merge(n)<-[r:RECEIVE]-(m) return r
   MATCH(n:PERSON),(m:PHONE) where n.phone = m.to  merge(n)-[c:CALL]->(m) return c
   MATCH(n:PERSON),(m:APPLY) where n.id = m.applicant  merge(n)<-[c:PERSON_INFO]-(m) return c
+  MATCH(n:PERSON),(m:BLACKINFO) where n.phone = m.number  merge(n)-[c:check_black]->(m) return c
   
 
